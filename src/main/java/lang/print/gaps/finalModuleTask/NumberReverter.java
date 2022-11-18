d93fa1489d12273fecc061fa929d792ee734a1b9
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-11-18 15:31:47 +0500

    done!

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,7 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public void revert(int number) {//498
+        System.out.println(number % 10 + "" + (number / 10) % 10 + "" + number / 100);
     }
 }
